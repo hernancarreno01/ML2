275704759855260fcabe519906a47d6f0b75abc1
hernancarreno01 <radioportatil@gmail.com>
2023-07-25 21:03:04 -0300

    botones

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3001, function() {
+    console.log("Server On");
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+});
